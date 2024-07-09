d75af802c27851339abba7a27af79016bca69cf4
saurabh4323 <saurabhiitr01@gmail.com>
2024-07-10 02:00:53 +0530

    change

--- config/database.js
+++ config/database.js
@@ -1,10 +1,19 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI).then(() => {
-        console.log('Database connected');
-    }).catch((error)=>{
-        console.log(error);
-    })
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      // Remove deprecated options
+      //   useNewUrlParser: true, // This can be removed
+      //   useUnifiedTopology: true, // This can be removed
+      // Remove deprecated options
+      // useCreateIndex: true,
+      // useFindAndModify: false
+    });
+    console.log("Database connected");
+  } catch (error) {
+    console.error("Database connection error:", error.message);
+  }
 };
+
 export default connectDB;
