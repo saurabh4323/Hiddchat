2b62917993bbf22f0448cbb3fe942fd169c41150
saurabh4323 <saurabhiitr01@gmail.com>
2024-07-10 11:19:34 +0530

    kya

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// const express = require('express')// method-1
-import express from "express"; // method-2
+import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
@@ -7,25 +6,28 @@ import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./socket/socket.js";
-dotenv.config({});
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-// middleware
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-const corsOption = {
-  origin: "http://localhost:3000",
-  credentials: true,
-};
-app.use(cors(corsOption));
 
-// routes
+const corsOptions = {
+  origin: ["https://hiddchatfrontend.vercel.app", "http://localhost:3000"],
+  credentials: true, // This is necessary for cookies to be sent across domains
+};
+
+app.use(cors(corsOptions));
+
+// Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/message", messageRoute);
 
 server.listen(PORT, () => {
   connectDB();
-  console.log(`Server listen at prot ${PORT}`);
+  console.log(`Server listening at port ${PORT}`);
 });
